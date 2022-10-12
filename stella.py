ef3cc685919650d4ff5554b3ad6712cb2ce4055b
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2022-10-12 20:06:22 +0200

    rimosso #!

--- stella.py
+++ stella.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import time              # Importazione di una libreria con funzioni speciali
 n=1                      # Dichiarazione di una variabile
 l=("","Uno","Due","Tre") # Dichiarazione di una t-upla (l'indice del primo elemento e' 0)
